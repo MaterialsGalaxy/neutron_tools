6e5bac0edc74c67500548db5938739df9c9aff4e
MilanSCD <milan.kumar@stfc.ac.uk>
2025-02-07 14:58:30 +0000

    add all gxhistory functions to HistoryModel class

--- dev_tools/interactive/interactivetool_dev_GSASII_refinement_demo/src/gxhistory.py
+++ dev_tools/interactive/interactivetool_dev_GSASII_refinement_demo/src/gxhistory.py
@@ -17,14 +17,101 @@ class HistoryModel:
         self.galaxy_url = galaxy_url
         self.api_key = api_key
         self.galaxy_instance = GalaxyInstance(url=galaxy_url, key=api_key)
+        self.history = None
+        self.table = None
+        self.latest_entry_id = None
+        self.gpx_choices = None
+        self.update()
+
+
+    def update(self):
         self.history = self.galaxy_instance.histories.show_history(
-            history_id=history_id,
+            history_id=self.history_id,
             contents=True,
             deleted=False,
             visible=True,
             types=["dataset"],
             keys=["Id", "Hid", "Name"],
         )
+        self.update_table()
+        self.update_gpx_choices()
+
+
+    def put(self, file_name: str, file_type: str = "auto"):
+        self.galaxy_instance.tools.upload_file(file_name, self.history_id)
+
+
+    def get_project(self, dataset_id: str, filep: str) -> None:
+        self.galaxy_instance.datasets.download_dataset(
+            dataset_id=dataset_id, file_path=filep, use_default_filename=False
+        )
+
+
+    def run_refinement(self, dataset_id: str, delta_id: str) -> None:
+        """runs the GSASII refinement: interactive executor tool in galaxy,
+        using a project file and a project delta as inputs to the executor tool
+
+        Args:
+            dataset_id (str): The galaxy api id of the gsas project to run the refinement on
+            delta_id (str): the galaxy api id of the delta to apply to the gsas project, before running the refinement.
+            the delta contains all parameter changes made in the interactive tool.
+        """
+
+        self.galaxy_instance.datasets.wait_for_dataset(delta_id)
+        input_data = {}
+        input_data["project"] = {"values": [{"src": "hda", "id": dataset_id}]}
+        input_data["delta"] = {"values": [{"src": "hda", "id": delta_id}]}
+        self.galaxy_instance.tools.run_tool(self.history_id, "gpx_gsas2", input_data)
+
+    def update_table(self) -> pd.DataFrame:
+        """gets the galaxy history from the galaxy instance and
+        creates a dataframe of the history entries with
+        History ids, names and Galaxy API ids.
+
+        Returns:
+            pd.DataFrame: dataframe of active entries in the galaxy history.
+        """
+        history_df: pd.DataFrame = pd.DataFrame(self.history)
+        self.table = history_df[["hid", "name", "id"]]
+
+        # set the latest entry id
+        row_id: int = self.table["hid"].idxmax()
+        self.latest_entry_id: str = self.table.loc[row_id, "id"]
+
+
+    def update_gpx_choices(self) -> dict:
+        """creates a dictionary of GSASII projects in the galaxy history
+        which can be loaded into the interactive tool.
+
+        Returns:
+            dict: dictionary of GSASII project files in the galaxy history
+            with keys of their Galaxy API IDs and values of
+            "history IDs: filename"
+        """
+        gpx_df = self.history_table[self.history_table["name"].str.endswith("gpx")]
+        gpx_choice_dict = dict(
+            [
+                (i, str(h) + ": " + fn)
+                for i, h, fn in zip(gpx_df["id"], gpx_df["hid"], gpx_df["name"])
+            ]
+        )
+
+        self.gpx_choices = dict(reversed(gpx_choice_dict.items()))
+
+    def run_generate_outputs(self, dataset_id: str) -> None:
+        """runs the GSASII refinement: output generator tool in galaxy using the selected GSASII project file. CIF and CSV files will be output into the Galaxy history.
+
+        Args:
+            dataset_id (str): galaxy API id of the current GSASII project used to generate the files.
+        """
+
+        input_data = {}
+        input_data["project"] = {"values": [{"src": "hda", "id": dataset_id}]}
+        self.galaxy_instance.tools.run_tool(self.history_id, "gpx_gsas2_output", input_data)
+        time.sleep(2)
+        self.update()
+        self.galaxy_instance.datasets.wait_for_dataset(self.latest_entry_id)
+
 
 def get_galaxy_connection():
     """connects to a galaxy isntance using the API Key and URL provided in the environment.
